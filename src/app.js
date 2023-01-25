04fae9034ec30f3c703fad6fef566616baf79b40
Aleryhu <aleryhuabdullahi@gmail.com>
2023-01-25 05:15:04 +0100

    Post requests

--- src/app.js
+++ src/app.js
@@ -3,24 +3,34 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 const PORT = 3000
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}...`)
 })
-
+const groceryList = [
+	{
+		item: 'milk',
+		quantity: 3,
+	},
+	{
+		item: 'cereal',
+		quantity: 2,
+	},
+	{
+		item: 'pop-tarts',
+		quantity: 1,
+	},
+]
 app.get('/groceries', (req, res) => {
-  res.send([
-    {
-      item:'milk',
-      quantity:3
-    },
-    {
-      item:'cereal',
-      quantity:2
-    },
-    {
-      item:'pop-tarts',
-      quantity:1
-    }
-  ])
+	console.log(groceryList)
+	res.send(groceryList)
+})
+
+app.post('/groceries', (req, res) => {
+	groceryList.push(req.body)
+	console.log(req.body)
+	res.sendStatus(201)
 })
